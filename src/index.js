1d1aa84fe8d3cb3eaf99627e57e9fdb8b8169cfa
Lizzhan <118141838+Lizzhan@users.noreply.github.com>
2023-10-22 20:20:33 -0700

    toggle on&off

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const dropBtn = document.getElementsByClassName('dropbtn')[0];
 const dropDown = document.getElementsByClassName('drop-down')[0];
+let isExpanded = false;
 
-function showDropDown() {
-    dropBtn.addEventListener('click',()=> { 
+function toggleDropDown() {
+    dropBtn.addEventListener('click',()=>{
+        if(isExpanded === false){
         dropDown.classList.remove('hidden');
+        isExpanded = true;
+        }else if(isExpanded === true){
+        dropDown.classList.add('hidden');
+        isExpanded = false;
+        };
     })
 }
 
-showDropDown();
+toggleDropDown();
